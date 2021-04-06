f409dcb64b69e8433f77c0f69ab69b2e6d689aaa
takumi <takumindex@gmail.com>
2021-04-06 14:11:30 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const getMedia = async() => {
+  const constraints = { audio: true, video: true };
+
+  try {
+    return await navigator.mediaDevices.getUserMedia(constraints);
+    /* ストリームを使用 */
+  } catch(err) {
+    /* エラーを処理 */
+    console.error(err);
+  }
+};
+
+getMedia();
+
+
+const App = () => {
+  return <div>Hello, React!</div>;
+};
+
+export default App;
