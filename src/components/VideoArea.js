6109afca53af6c7b996ca0acfac4241d0379cf8a
takumi <takumindex@gmail.com>
2021-04-10 17:23:33 +0900

    commit

--- src/components/VideoArea.js
+++ src/components/VideoArea.js
@@ -25,7 +25,7 @@ const VideoArea = ({ localPeerName, RemotePeerName }) => {
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={6}>
-          <VideoLocal localPeerName={localPeerName} />
+          <VideoLocal name={localPeerName} />
         </Grid>
         <Grid item xs={12} sm={6}>
         </Grid>
