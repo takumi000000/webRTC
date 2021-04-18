13fcbd3962d11efd051c5041aab52067f7b23704
takumi000000 <taakumi.sabuakadesu@gmail.com>
2021-04-18 15:03:57 +0900

    commit

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { 
+  Card, 
+  CardActionArea,
+  CardActions,
+  CardContent,
+  Typography,
+} from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
 
 const Video = ({ isLocal, name, videoRef }) => {
+  const classes = useStyles();
+
+  const V = ({ isLocal, name, videoRef }) => {
+    return (
+      <div>
+        <video autoPlay muted={isLocal} ref={videoRef} />
+        <div>{name}</div>
+      </div>
+      )
+  };
+
   return (
-    <div>
+    <Card className={classes.root}>
+      <CardActionArea>
       <video autoPlay muted={isLocal} ref={videoRef} />
-      <div>{name}</div>
-    </div>
-    )
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {name}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions></CardActions>
+    </Card>
+  );
 };
 
 export default Video;
