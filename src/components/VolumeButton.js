21553015fecbe3d75a0abc4c0e377e2deece11e0
takumi000000 <taakumi.sabuakadesu@gmail.com>
2021-04-20 22:42:50 +0900

    commit

---
+++ src/components/VolumeButton.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import VolumeOffIcon from '@material-ui/icons/VolumeOff';
+import VolumeUpIcon from '@material-ui/icons/VolumeUp';
+
+const VolumeButton = ({ muted, setMuted }) => {
+  const Icon = muted ? VolumeOffIcon : VolumeUpIcon;
+  return (
+    <IconButton 
+    aria-label="switch mute" 
+    onClick={() => {
+      setMuted((previousState) => !previousState);      // !は逆転する
+    }}
+    >
+    <Icon />
+  </IconButton>
+  );
+};
+
+export default VolumeButton;
